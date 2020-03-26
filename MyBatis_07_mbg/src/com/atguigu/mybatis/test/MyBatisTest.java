feb21bf84cfae05f73c444621d3be388db2efaa2
tom_glb <ASUS@LAPTOP-J89KJ169>
2020-03-26 17:12:44 +0800

    mybatis

---
+++ MyBatis_07_mbg/src/com/atguigu/mybatis/test/MyBatisTest.java
@@ -0,0 +1,50 @@
+package com.atguigu.mybatis.test;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+
+
+/*import com.atguigu.mybatis.bean.Employee;
+import com.atguigu.mybatis.dao.EmployeeMapper;*/
+
+public class MyBatisTest {
+
+	public SqlSessionFactory getSqlSessionFactory() throws IOException {
+		String resource = "mybatis-config.xml";
+		InputStream inputStream = Resources.getResourceAsStream(resource);
+		return new SqlSessionFactoryBuilder().build(inputStream);
+	}
+	@Test
+	public void test() throws IOException, XMLParserException, InvalidConfigurationException, SQLException, InterruptedException{
+		 List<String> warnings = new ArrayList<String>();
+		   boolean overwrite = true;
+		   File configFile = new File("mbg.xml");
+		   ConfigurationParser cp = new ConfigurationParser(warnings);
+		   Configuration config = cp.parseConfiguration(configFile);
+		   DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+		   MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+		   myBatisGenerator.generate(null);
+		
+	}
+
+	
+	
+
+}
